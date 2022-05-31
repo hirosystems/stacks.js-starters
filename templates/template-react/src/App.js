d7df5d0e37ceb2f36461fb19d6585d768c7f35e3
janniks <janniks@users.noreply.github.com>
2022-05-31 16:00:18 +0200

    Add stacks.js to template-react

--- templates/template-react/src/App.js
+++ templates/template-react/src/App.js
@@ -1,19 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import ConnectWallet from "./components/ConnectWallet";
+import ContractCallVote from "./components/ContractCallVote";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+
+        <h2>React + Stacks.js 👋</h2>
+
+        {/* ConnectWallet file: `./src/components/ConnectWallet.js` */}
+        <ConnectWallet />
+
+        {/* ContractCallVote file: `./src/components/ContractCallVote.js` */}
+        <ContractCallVote />
+
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
+          href="https://docs.hiro.so/intro"
           rel="noopener noreferrer"
+          target="_blank"
+        >
+          Learn Stacks
+        </a>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          rel="noopener noreferrer"
+          target="_blank"
         >
           Learn React
         </a>
--- templates/template-react/src/index.js
+++ templates/template-react/src/index.js
@@ -1,17 +1,33 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+import { Connect } from "@stacks/connect-react";
+import { Buffer } from "@stacks/common";
+
+import { userSession } from "./components/ConnectWallet";
+
+global.Buffer = Buffer;
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <Connect
+      authOptions={{
+        appDetails: {
+          name: "Stacks React Template",
+          // todo:
+          icon: window.location.origin + "/logo.png",
+        },
+        redirectTo: "/",
+        onFinish: () => {
+          window.location.reload();
+        },
+        userSession,
+      }}
+    >
+      <App />
+    </Connect>
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
