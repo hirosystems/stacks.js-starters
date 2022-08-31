c4f49b55bd4d6b3a8d955b8381c5331e1bf98ad3
janniks <janniks@users.noreply.github.com>
2022-08-31 13:57:03 +0200

    chore: update stacks.js except for angular

--- templates/template-angular/src/main.ts
+++ templates/template-angular/src/main.ts
@@ -4,6 +4,10 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
 
+import { Buffer } from '@stacks/common';
+
+(window as any).Buffer = (window as any).Buffer || Buffer;
+
 if (environment.production) {
   enableProdMode();
 }
