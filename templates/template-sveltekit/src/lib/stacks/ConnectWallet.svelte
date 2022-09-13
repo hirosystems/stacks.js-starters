752dbe52d66f500de0ca1f7b2e5a480158e8e241
janniks <janniks@users.noreply.github.com>
2022-09-13 21:30:32 +0200

    fix: add sveltekit template

---
+++ templates/template-sveltekit/src/lib/stacks/ConnectWallet.svelte
@@ -0,0 +1,47 @@
+<script>
+  import { showConnect } from "@stacks/connect";
+  import { userSession } from "$lib/stacksUserSession";
+
+  export function authenticate() {
+    showConnect({
+      appDetails: {
+        name: "Stacks Svelte Starter",
+        icon: window.location.origin + "/svelte.png",
+      },
+      redirectTo: "/",
+      onFinish: () => {
+        window.location.reload();
+      },
+      userSession,
+    });
+  }
+
+  export function disconnect() {
+    userSession.signUserOut("/");
+  }
+</script>
+
+<div>
+  {#if userSession.isUserSignedIn()}
+    <button on:click={disconnect}> Disconnect Wallet </button>
+    <p>mainnet: {userSession.loadUserData().profile.stxAddress.mainnet}</p>
+    <p>testnet: {userSession.loadUserData().profile.stxAddress.testnet}</p>
+  {:else}
+    <button on:click={authenticate}> Connect Wallet </button>
+  {/if}
+</div>
+
+<style>
+  div {
+    margin-top: 8px;
+  }
+
+  button {
+    margin: 8px;
+    background-color: #bbb;
+    border: 2px solid #777;
+    border-radius: 28px;
+    font-size: 18px;
+    padding: 16px 24px;
+  }
+</style>
---
+++ templates/template-sveltekit/src/routes/+layout.svelte
@@ -0,0 +1,69 @@
+<script>
+  import Counter from "$lib/Counter.svelte";
+  import Header from "$lib/header/Header.svelte";
+  import "../app.css";
+
+  import { Buffer } from "@stacks/common";
+  if (typeof window !== "undefined") window.Buffer = window.Buffer || Buffer;
+</script>
+
+<Header />
+
+<main>
+  <slot />
+</main>
+
+<footer>
+  <p>
+    visit <a href="https://docs.hiro.so" class="stacks">docs.hiro.so</a> to
+    learn more about developing on Stacks
+    <br />
+    visit
+    <a href="https://github.com/hirosystems/stacks.js" class="stacks"
+      >the official Stacks.js repo</a
+    >
+    for more examples
+    <br />
+    visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to learn SvelteKit
+  </p>
+
+  <div class="counter">
+    SvelteKit Example Component
+    <Counter />
+  </div>
+</footer>
+
+<style>
+  main {
+    flex: 1;
+    display: flex;
+    flex-direction: column;
+    padding: 1rem;
+    width: 100%;
+    max-width: 1024px;
+    margin: 0 auto;
+    box-sizing: border-box;
+  }
+
+  footer {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    padding: 40px;
+  }
+
+  footer a {
+    font-weight: bold;
+  }
+
+  footer a.stacks {
+    color: #7532d9;
+  }
+
+  @media (min-width: 480px) {
+    footer {
+      padding: 40px 0;
+    }
+  }
+</style>
