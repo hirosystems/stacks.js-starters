72dfa66046031a032fa001b31c977ee267e14060
janniks <janniks@users.noreply.github.com>
2022-09-13 22:16:38 +0200

    chore: update react templates

---
+++ templates/template-react-vite/src/components/ContractCallVote.jsx
@@ -0,0 +1,55 @@
+import { useConnect } from "@stacks/connect-react";
+import { StacksTestnet } from "@stacks/network";
+import {
+  AnchorMode,
+  PostConditionMode,
+  stringUtf8CV,
+} from "@stacks/transactions";
+import { userSession } from "./ConnectWallet";
+
+const ContractCallVote = () => {
+  const { doContractCall } = useConnect();
+
+  function vote(pick) {
+    doContractCall({
+      network: new StacksTestnet(),
+      anchorMode: AnchorMode.Any,
+      contractAddress: "ST39MJ145BR6S8C315AG2BD61SJ16E208P1FDK3AK",
+      contractName: "example-fruit-vote-contract",
+      functionName: "vote",
+      functionArgs: [stringUtf8CV(pick)],
+      postConditionMode: PostConditionMode.Deny,
+      postConditions: [],
+      onFinish: (data) => {
+        console.log("onFinish:", data);
+        window
+          .open(
+            `https://explorer.stacks.co/txid/${data.txId}?chain=testnet`,
+            "_blank"
+          )
+          .focus();
+      },
+      onCancel: () => {
+        console.log("onCancel:", "Transaction was canceled");
+      },
+    });
+  }
+
+  if (!userSession.isUserSignedIn()) {
+    return null;
+  }
+
+  return (
+    <div>
+      <p>Vote via Smart Contract</p>
+      <button className="Vote" onClick={() => vote("🍊")}>
+        Vote for 🍊
+      </button>
+      <button className="Vote" onClick={() => vote("🍎")}>
+        Vote for 🍎
+      </button>
+    </div>
+  );
+};
+
+export default ContractCallVote;
---
+++ templates/template-react-vite/src/main.jsx
@@ -0,0 +1,33 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+
+import { Connect } from "@stacks/connect-react";
+import { Buffer } from "@stacks/common";
+
+import { userSession } from "./components/ConnectWallet";
+
+window.Buffer = Buffer;
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <React.StrictMode>
+    <Connect
+      authOptions={{
+        appDetails: {
+          name: "Stacks React Template",
+          // todo:
+          icon: window.location.origin + "/logo.png",
+        },
+        redirectTo: "/",
+        onFinish: () => {
+          window.location.reload();
+        },
+        userSession,
+      }}
+    >
+      <App />
+    </Connect>
+  </React.StrictMode>
+);
