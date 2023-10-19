005577190c0d30797a588fb9af01924c76a13c85
janniks <janniks@users.noreply.github.com>
2023-10-19 13:44:26 +0100

    chore: add template-sveltekit-ts

---
+++ templates/template-sveltekit-ts/src/routes/+page.svelte
@@ -0,0 +1,29 @@
+<script lang="ts">
+  import ConnectWallet from "$lib/ConnectWallet.svelte";
+  import ContractVote from "$lib/ContractVote.svelte";
+</script>
+
+<h1>Svelte + Stacks.js 👋</h1>
+
+<!-- ConnectWallet file: `./src/lib/ConnectWallet.svelte` -->
+<ConnectWallet />
+
+<!-- ContractVote file: `./src/lib/ContractVote.svelte` -->
+<ContractVote />
+
+<p>
+  Visit the Stacks.js
+  <a
+    class="Stacks"
+    target="_blank"
+    href="https://github.com/hirosystems/stacks.js">official repository</a
+  >
+  and <a class="Stacks" href="https://stacks.js.org">library reference</a> for more
+  information.
+</p>
+
+<hr />
+
+<p>
+  Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation
+</p>
