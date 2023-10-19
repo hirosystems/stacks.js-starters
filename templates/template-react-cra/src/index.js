011eeffd973c17dc8be236694606e1e6662a1b77
janniks <janniks@users.noreply.github.com>
2023-10-19 11:40:29 +0100

    chore: install from npx create-react-app

--- templates/template-react-cra/src/index.js
+++ templates/template-react-cra/src/index.js
@@ -1,30 +1,17 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import "./index.css";
-import App from "./App";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
 
-import { Connect } from "@stacks/connect-react";
-
-import { userSession } from "./components/ConnectWallet";
-
-const root = ReactDOM.createRoot(document.getElementById("root"));
+const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Connect
-      authOptions={{
-        appDetails: {
-          name: "Stacks React Template",
-          // todo:
-          icon: window.location.origin + "/logo.png",
-        },
-        redirectTo: "/",
-        onFinish: () => {
-          window.location.reload();
-        },
-        userSession,
-      }}
-    >
-      <App />
-    </Connect>
+    <App />
   </React.StrictMode>
 );
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
